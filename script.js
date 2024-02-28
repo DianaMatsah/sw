ce2f20a948425f1f8fba71c981ff9ef3df7b973a
DianaMatsah <161348104+DianaMatsah@users.noreply.github.com>
2024-02-28 18:35:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var whatsappButton = document.getElementById("whatsappButton");
+
+whatsappButton.addEventListener("click", function() {
+    window.location.href = "https://wa.me/qr/AV7ROWCL2Q7AC1";
+});
+
+var instagramButton = document.getElementById("instagramButton");
+
+instagramButton.addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/safiyyahs__?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
+});
+
+var albumImageA = document.getElementById("albumImageA");
+var albumImageB = document.getElementById("albumImageB");
